99f6bac1ea92f771ad210b33096998bf52863712
Timur <timur3424242@gmail.com>
2025-06-18 18:30:20 +0300

    v. 001

--- shooter_game.py
+++ shooter_game.py
@@ -1,7 +1,4 @@
-from math import gamma
-
 from pygame import *
-
 import sys
 import os
 
@@ -12,6 +9,35 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(resource_path(player_image)), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def move(self):
+        keys_pressed = key.get_pressed()
+
+        if keys_pressed[K_RIGHT] and self.rect.x < screen_width:
+            self.rect.x += self.speed
+            self.image = transform.scale(image.load(resource_path('rocket.png')), (60, 60))
+        if keys_pressed[K_LEFT] and self.rect.x > screen_width:
+            self.image = transform.scale(image.load(resource_path('rocket.png')), (60, 60))
+            self.rect.x -= self.speed
+
+class Enemy(GameSprite):
+    def move(self):
+        if self.rect.y < screen_height:
+            self.rect.y -= self.speed
+            self.image = transform.scale(image.load(resource_path('cyborgL.png')), (60, 60))
 
 font.init()
 font = font.Font(None, 70)
@@ -40,10 +66,18 @@ FPS = 60
 
 mixer.init()
 mixer.music.load('space.ogg')
-mixer.music.play()
+#mixer.music.play()
+
+seredina = screen_width / 2
+
+player = Player('rocket.png',screen_height,seredina,10)
 
 game = True
 
 while game == True:
+    window.blit(background, (0, 0))
+    player.reset()
+    player.move()
+
     display.update()
     clock.tick(FPS)
